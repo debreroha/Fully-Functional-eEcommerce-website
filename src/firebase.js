febeb688c259cc9a59b96e42d9ffc483331b87d2
Getnet Alemye <debreroha12@gmail.com>
2024-01-12 17:13:09 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDW5gOoH4dsOWxzNoPLG0RJrdyuWkLISA0",
+  authDomain: "challenge-c887b.firebaseapp.com",
+  projectId: "challenge-c887b",
+  storageBucket: "challenge-c887b.appspot.com",
+  messagingSenderId: "759632979873",
+  appId: "1:759632979873:web:05c71545e5a19879414de0",
+  measurementId: "G-PM5C4N7S38"
+};
